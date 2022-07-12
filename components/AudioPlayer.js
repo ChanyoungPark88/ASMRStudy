51e6edda3cfe00cf9decaee08f1d5f419609ac58
ChanyoungPark88 <chanyoung.p.1988@gmail.com>
2022-07-12 01:22:19 -0600

    add volume progressbar and chatroom button

--- components/AudioPlayer.js
+++ components/AudioPlayer.js
@@ -1,12 +1,14 @@
-import React, { useState, useRef, useEffect } from 'react';
+import React, { useState, useRef } from 'react';
 import styles from '../styles/Home.module.css';
 import { FaPlay, FaPause, FaBackward, FaForward } from 'react-icons/fa';
 
 const AudioPlayer = () => {
   // state
   const [isPlaying, setIsPlaying] = useState(false);
+
   // references
   const audioPlayer = useRef();
+  const progressBar = useRef();
 
   const togglePlayPause = () => {
     const prevValue = isPlaying;
@@ -18,18 +20,40 @@ const AudioPlayer = () => {
     }
   };
 
+  const changeVolume = (e) => {
+    audioPlayer.current.volume = e.target.value / 100;
+  };
+
   return (
-    <div className={styles.container}>
-      <audio ref={audioPlayer} src='../musics/rain.mp3'></audio>
-      {/* <button className={styles.button}>
-        <FaBackward />
-      </button> */}
-      <button className={styles.button} onClick={togglePlayPause}>
-        {isPlaying ? <FaPause /> : <FaPlay />}
-      </button>
-      {/* <button className={styles.button}>
-        <FaForward />
-      </button> */}
+    <div>
+      <div className={styles.container}>
+        <audio
+          ref={audioPlayer}
+          src='https://www.soundjay.com/nature/rain-03.mp3'
+          preload='metadata'
+          loop
+          volume
+        ></audio>
+        <button className={styles.button} onClick={togglePlayPause}>
+          {isPlaying ? <FaPause /> : <FaPlay />}
+        </button>
+      </div>
+
+      <div className={styles.container}>
+        <input
+          type='range'
+          className={styles.progressBar}
+          defaultValue='50'
+          ref={progressBar}
+          onChange={changeVolume}
+        />
+      </div>
+
+      <div className={styles.container}>
+        <button className={styles.chatButton}>
+          <span>Chat Room</span>
+        </button>
+      </div>
     </div>
   );
 };
